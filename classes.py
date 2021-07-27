b0d23e8a89966cb3c6b35bf2bb97ac1023fd9ec5
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2021-07-27 08:14:12 +0000

    Add function to create the intro Branches object

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 # Classes containing text and player options
 # for most of the story
 
@@ -15,3 +18,23 @@ class Branches:
         self.opt_txt = opt_txt
         self.exits = exits
         self.exit_txt = exit_txt
+
+
+def create_intro():
+    """
+    Create the intro Branches object
+    """
+    int_txt = inspect.cleandoc("""
+    ""The Forest of High Elms needs your help.
+    The Evil Sourcerer has poisoned the Tree of Life and the forest is dying.
+    He has locked the antidote in his castle"" """)
+    choice_txt = '"Will you embark on a quest to retrieve it for us?"'
+    options = ['yes', 'no']
+    opt_txt = 'Do you help?'
+    exits = ['ITEM_ONE', 'INTRO_NO']
+    exit_txt = []
+    exit_txt.append('"Thank you, I know you will save our forest."')
+    exit_txt.append('"No?! What a big coward you are!"')
+
+    intro = Branches(int_txt, choice_txt, options, opt_txt, exits, exit_txt)
+    return intro
--- run.py
+++ run.py
@@ -1,1 +1,6 @@
 import classes
+
+
+test = classes.create_intro()
+print(f'{test.int_txt}\n')
+print(f'{test.choice_txt}\n')
